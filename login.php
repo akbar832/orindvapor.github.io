1fede36afbe51f3b247cea8f84de1a6c30e62885
akbar832 <raihanakbar832@gmail.com>
2024-06-28 09:36:22 +0700

    Update html

--- login.php
+++ login.php
@@ -1,24 +1,24 @@
 <?php
-@ob_start();
-session_start();
-if (isset($_POST['proses'])) {
-    require 'config.php';
+	@ob_start();
+	session_start();
+	if(isset($_POST['proses'])){
+		require 'config.php';
+			
+		$user = strip_tags($_POST['user']);
+		$pass = strip_tags($_POST['pass']);
 
-    $user = strip_tags($_POST['user']);
-    $pass = strip_tags($_POST['pass']);
-
-    $sql = 'SELECT member.*, login.user, login.pass
-            FROM member INNER JOIN login ON member.id_member = login.id_member
-            WHERE user =? AND pass = (?)';
-    $row = $config->prepare($sql);
-    $row->execute(array($user, $pass));
-    $jum = $row->rowCount();
-    if ($jum > 0) {
-        $hasil = $row->fetch();
-        $_SESSION['admin'] = $hasil;
-        echo json_encode(['status' => 'success', 'message' => 'Login Sukses']);
-    } else {
-        echo json_encode(['status' => 'error', 'message' => 'Login Gagal']);
-    }
-}
+		$sql = 'select member.*, login.user, login.pass
+				from member inner join login on member.id_member = login.id_member
+				where user =? and pass = (?)';
+		$row = $config->prepare($sql);
+		$row -> execute(array($user,$pass));
+		$jum = $row -> rowCount();
+		if($jum > 0){
+			$hasil = $row -> fetch();
+			$_SESSION['admin'] = $hasil;
+			echo '<script>alert("Login Sukses");window.location="index.php"</script>';
+		}else{
+			echo '<script>alert("Login Gagal");history.go(-1);</script>';
+		}
+	}
 ?>
